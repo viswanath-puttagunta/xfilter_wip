c4b7e3a7b7c3ac2f17a9dbd138e7862596881810
Viswanath Puttagunta <vishp100@gmail.com>
2017-03-26 11:06:13 -0500

    xfilter_wip: Basic web page

---
+++ nasdaq/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NasdaqConfig(AppConfig):
+    name = 'nasdaq'
---
+++ xfilter_wip/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for xfilter_wip project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xfilter_wip.settings")
+
+application = get_wsgi_application()
